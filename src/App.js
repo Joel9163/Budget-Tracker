e281a66c09de7114a35d6dbaf613054db9f0fb97
Joel Fu <73356528+Joel9163@users.noreply.github.com>
2024-01-06 20:30:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,31 +11,31 @@ function App () {
   return (
       <AppProvider>
           <div className='container'>
-          <h1 className='mt-3'>Expense Tracker</h1>
-          <div className='row mt-3'>
-					  <div className='col-sm'>
-						  <Budget />
-					  </div>
-					  <div className='col-sm'>
-						  <RemainingBudget />
-					  </div>
-					  <div className='col-sm'>
-						  <Expenses />
-					  </div>
-          </div>
-          <h3 className ='mt-3'> Expenses</h3>
-      <div className= 'row mt-3'>
-        <div className='col-sm'>
-          <ExpenseList />
-				</div>
-      </div>
-      <h3 className ='mt-3'> Add Expenses</h3>
-      <div className= 'row mt-3'>
-        <div className='col-sm'>
-          <AddExpense />
-				</div>
-      </div>
-    </div>
+          	<h1 className='mt-3'>Expense Tracker</h1>
+          	<div className='row mt-3'>
+	  		<div className='col-sm'>
+	  			<Budget />
+	  		</div>
+	  		<div className='col-sm'>
+	  			<RemainingBudget />
+	  		</div>
+	  		<div className='col-sm'>
+	  			<Expenses />
+	  		</div>
+	  	</div>
+          	<h3 className ='mt-3'> Expenses</h3>
+	  	<div className= 'row mt-3'>
+	  		<div className='col-sm'>
+          			<ExpenseList />
+			</div>
+      		</div>
+      		<h3 className ='mt-3'> Add Expenses</h3>
+      		<div className= 'row mt-3'>
+        		<div className='col-sm'>
+          			<AddExpense />
+			</div>
+      		</div>
+    	</div>
       </AppProvider>
   );
 };
